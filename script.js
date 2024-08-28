95b562cce9807a4d08627e9a6ca17c13bb9ec9cd
CC3636-IV <johansen.morten.m@gmail.com>
2024-08-28 12:10:14 +0200

    progress with JS inrodution

---
+++ script.js
@@ -0,0 +1,30 @@
+let greeting = "Hello kodelofte"
+console.log (greeting);
+
+greeting = "helt annet"
+console.log (greeting);
+//kommentar test
+
+let a = 10
+let b = 32
+let c = a + b
+console.log (c)
+
+let lars = {
+    name: "Lars Gunnar",
+    age: 35,
+    alive: true,
+}
+console.log(lars)
+
+console.log(lars.name)
+
+
+if(lars.alive)
+{
+  console.log("Hes alive!")
+}
+else
+{
+    console.log("Hes dead!")
+}
